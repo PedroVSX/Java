144b3f4bb6c276793e72cc1898b73fccf4a666fd
Pedro Vieira <160655744+PedroVSX@users.noreply.github.com>
2024-02-22 11:07:39 -0300

    Update ListaEstatica.java

--- EstruturaDeDados/ListaEstatica/ListaEstatica.java
+++ EstruturaDeDados/ListaEstatica/ListaEstatica.java
@@ -2,13 +2,12 @@ package ListaEstatica;
 
 public class ListaEstatica {
 
-    private int[] vetor = new int[0];
+    private Integer[] vetor = new Integer[0];
     private int cont = 0;
 
     public void add(int elem) {
         if (cont == vetor.length) {
             aumentaArray();
-
         }
 
         vetor[cont] = elem;
@@ -16,15 +15,67 @@ public class ListaEstatica {
 
     }
 
+    public void add(int pos, int elem) {
+        aumentaArray();
+
+
+    }
+
+    public int size() {
+        return cont;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean contains(Object obj) {
+        for (int i = 0; i < cont; i++) {
+            if (vetor[i] == obj) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void remove(Object obj) {
+
+    }
+
+    public void remove(int pos) {
+        Integer[] novaLista = new Integer[vetor.length - 1];
+
+        for (int i = 0; i < cont; i++) {
+            if (i != pos) {
+                novaLista[i] = vetor[i];
+            }
+        }
+
+        vetor = novaLista;
+    }
+
+    public int get(int pos) {
+        return vetor[pos];
+    }
+
+    public int indexOf(Object obj) {
+        for (int i = 0; i < cont; i++) {
+            if (vetor[i] == obj) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public void show() {
         System.out.print("[");
         for (int i = 0; i < cont; i++) {
             if(i == cont - 1) {
                 System.out.print(vetor[i]);
-
             } else {
                 System.out.printf("%s, ", vetor[i]);
-
             }
 
         }
@@ -34,11 +85,10 @@ public class ListaEstatica {
     }
 
     private void aumentaArray() {
-        int[] novoVetor = new int[vetor.length + 1];
+        Integer[] novoVetor = new Integer[vetor.length + 1];
 
         for (int i = 0; i < vetor.length; i++) {
             novoVetor[i] = vetor[i];
-
         }
 
         vetor = novoVetor;
@@ -46,4 +96,3 @@ public class ListaEstatica {
     }
 
 }
-
