1aea5017a30638502f4419285fe797added820e6
Pedro Vieira <160655744+PedroVSX@users.noreply.github.com>
2024-03-04 17:07:45 -0300

    Update ListaDinamica.java

--- EstruturaDeDados/ListaDinamica/ListaDinamica.java
+++ EstruturaDeDados/ListaDinamica/ListaDinamica.java
@@ -1,3 +1,5 @@
+package ListaDinamica;
+
 public class ListaDinamica {
 
     private Node primeiro;
@@ -8,7 +10,7 @@ public class ListaDinamica {
     public void add(Object elem) {
         Node novo = new Node(elem);
 
-        if (primeiro.dado == null){
+        if (this.primeiro == null){
             this.primeiro = novo;
             this.ultimo = novo;
         } else {
@@ -38,31 +40,109 @@ public class ListaDinamica {
     }
 
     public void clear() {
-
+        cont = 0;
+        primeiro = null;
+        ultimo = null;
     }
 
     public boolean contains(Object obj) {
-        return true;    
+        Node aux = primeiro;
+
+        for (int i = 0; i < cont; i++) {
+            if (aux.dado == obj) {
+                return true;
+            }
+            aux = aux.proximo;
+        }
+
+        return false;
     }
 
     public void remove(Object obj) {
+        Node aux = primeiro;
+        boolean found = false;
 
+        if (primeiro.dado == obj) {
+            primeiro = primeiro.proximo;
+            found = true;
+        } else {
+            for (int i = 0; i < cont - 1; i++) {
+                if (aux.proximo.dado == obj) {
+                    aux.proximo = aux.proximo.proximo;
+                    found = true;
+                } else {
+                    aux = aux.proximo;
+                    ultimo.proximo = aux;
+                    ultimo = aux;
+                }
+            }
+        }
+
+        if (found) {
+            cont--;
+        }
     }
 
     public void remove(int pos) {
+        Node aux = primeiro;
+        boolean found = false;
 
+        if (pos == 0) {
+            primeiro = primeiro.proximo;
+            found = true;
+        } else {
+            for (int i = 0; i < cont - 1; i++) {
+                if (i+1 == pos) {
+                    aux.proximo = aux.proximo.proximo;
+                    found = true;
+                } else {
+                    aux = aux.proximo;
+                    ultimo.proximo = aux;
+                    ultimo = aux;
+                }
+            }
+        }
+
+        if (found) {
+            cont--;
+        }
     }
 
     public Object get(int pos) {
-        return "oi";
+        Node aux = primeiro;
+
+        if (pos > cont-1 || pos < 0) {
+            throw new IndexOutOfBoundsException("Index " + pos + " out of bounds for length " + cont);
+        }
+
+        for (int i = 0; i < cont; i++) {
+            if (i == pos) {
+                return aux.dado;
+            } else {
+                aux = aux.proximo;
+            }
+        }
+
+        return 0;
     }
 
     public int indexOf(Object obj) {
+        Node aux = primeiro;
+
+        for (int i = 0; i < cont; i++) {
+            if (aux.dado == obj) {
+                return i;
+            } else {
+                aux = aux.proximo;
+            }
+        }
+
         return -1;
     }
 
     public void show() {
-        Node aux = new Node(primeiro.dado);
+        Node aux = new Node(this.primeiro.dado);
+        aux.proximo = this.primeiro.proximo;
 
         System.out.print("[");
         for (int i = 0; i < cont; i++) {
@@ -70,15 +150,9 @@ public class ListaDinamica {
                 System.out.printf("%s",aux.dado);
             } else {
                 System.out.printf("%s, ", aux.dado);
-                aux = primeiro.proximo;
+                aux = aux.proximo;
             }
         }
         System.out.println("]");
     }
-
-    private void aumentaArray() {
-
-    }
-
-
 }
