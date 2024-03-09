4dbc5e1ee5cca9838de82558db3a40fcdded15c1
Pedro Vieira <160655744+PedroVSX@users.noreply.github.com>
2024-03-09 13:51:26 -0300

    Update Node.java

--- EstruturaDeDados/ListaDinamica/Node.java
+++ EstruturaDeDados/ListaDinamica/Node.java
@@ -2,10 +2,12 @@ public class Node {
 
     Object dado;
     Node proximo;
+    Node anterior;
 
     public Node(Object dado) {
         this.dado = dado;
         this.proximo = null;
+        this.anterior = null;
     }
 
 }
